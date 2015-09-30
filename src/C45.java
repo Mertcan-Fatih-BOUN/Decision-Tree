c2585b207f9b5423f25ccd828ae5b2d70e0ffe21
Mert Can Çüçen <mertcancucen@gmail.com>
2015-10-01 00:28:18 +0300

    Base Cases Done Best Gain to-do

--- src/C45.java
+++ src/C45.java
@@ -3,6 +3,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by mertcan on 30.9.2015.
@@ -10,43 +11,84 @@ import java.util.ArrayList;
 public class C45 {
 
     public static ArrayList<Instance> instances = new ArrayList<Instance>();
+    public static String mostFreqClass;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
             readDataSet("iris.data.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
+        mostFreqClass = findMostFreqClass(instances);
+        System.out.println(mostFreqClass);
 
         createDecisionTree(instances);
     }
 
-    private static void createDecisionTree(ArrayList<Instance> instances) {
-        checkBase1();
-        checkBase2();
-        checkBase3();
-        findBestGain();
-        formTree();
+    private static String findMostFreqClass(ArrayList<Instance> instances) {
+        HashMap<String, Integer> names = new HashMap<String, Integer>();
+        String freqName = "";
+        int freq = 0;
+        for (int i = 0; i < instances.size(); i++) {
+            if (names.get(instances.get(i).className) == null) {
+                names.put(instances.get(i).className, 1);
+                if (freq == 0) {
+                    freqName = instances.get(i).className;
+                    freq = 1;
+                }
+            } else {
+                names.put(instances.get(i).className, names.get(instances.get(i).className) + 1);
+                if (freq < names.get(instances.get(i).className)) {
+                    freqName = instances.get(i).className;
+                    freq = names.get(instances.get(i).className);
+                }
+            }
+        }
+        return freqName;
     }
 
-    private static void checkBase1() {
-
+    private static String createDecisionTree(ArrayList<Instance> instances) {
+        if (checkEmptiness(instances)) {
+            return "failure";
+        } else if (checkAllSame(instances)) {
+            return instances.get(0).className;
+        }  else if (checkEmptyAttribute(instances)) {
+            return mostFreqClass;
+        } else {
+            findBestGain();
+            return formTree();
+        }
     }
 
-    private static void checkBase2() {
-
+    private static boolean checkAllSame(ArrayList<Instance> instances) {
+        String temp = instances.get(0).className;
+        for (int i = 1; i < instances.size(); i++) {
+            if (!instances.get(i).className.equals(temp))
+                return false;
+        }
+        return true;
     }
 
-    private static void checkBase3() {
+    private static boolean checkEmptiness(ArrayList<Instance> instances) {
+        if (instances.size() > 0)
+            return true;
+        else
+            return false;
+    }
 
+    private static boolean checkEmptyAttribute(ArrayList<Instance> instances) {
+        if (instances.get(0).attributes.size() == 0)
+            return true;
+        else
+            return false;
     }
 
     private static void findBestGain() {
 
     }
 
-    private static void formTree() {
-
+    private static String formTree() {
+        return "tree";
     }
 
     private static void readDataSet(String s) throws IOException {
@@ -55,11 +97,11 @@ public class C45 {
 
         String strLine;
 
-        while ((strLine = br.readLine()) != null)   {
+        while ((strLine = br.readLine()) != null) {
             String[] parts = strLine.split(",");
             Instance instance = new Instance(parts[parts.length - 1]);
-            for(int i = 0; i < parts.length - 1; i++){
-                instance.attributes.add(Integer.parseInt(parts[i]));
+            for (int i = 0; i < parts.length - 1; i++) {
+                instance.attributes.add(Double.parseDouble(parts[i]));
             }
             instances.add(instance);
         }
@@ -67,15 +109,16 @@ public class C45 {
         br.close();
     }
 
-    public static class Instance{
-        public ArrayList<Integer> attributes;
+    public static class Instance {
+        public ArrayList<Double> attributes;
         public String className;
 
-        public Instance(String name){
-            attributes = new ArrayList<Integer>();
+        public Instance(String name) {
+            attributes = new ArrayList<Double>();
             className = name;
         }
-        public Instance(String name, ArrayList<Integer> atts){
+
+        public Instance(String name, ArrayList<Double> atts) {
             attributes = atts;
             className = name;
         }
