e897d3f685b2c404d68f20be4dd1a698b524f01b
Mert Can Çüçen <mertcancucen@gmail.com>
2015-11-25 02:03:09 +0200

    Fix after merge

--- src/SDT/Node.java
+++ src/SDT/Node.java
@@ -1,6 +1,8 @@
 package SDT;
 
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -18,6 +20,7 @@ class Node {
     boolean isLeft;
     double w0;
     double[] w;
+    static boolean hardInit = false;
 
     double y;
     double g;
@@ -132,11 +135,15 @@ class Node {
 
         double alpha;
         for (int t = 0; t < tree.MAX_STEP; t++) {
-            for (int i = 0; i < ATTRIBUTE_COUNT; i++)
-                w[i] = rand(-0.005, 0.005);
-            w0 = rand(-0.005, 0.005);
-            leftNode.w0 = rand(-0.005, 0.005);
-            rightNode.w0 = rand(-0.005, 0.005);
+            if(hardInit)
+                hardinit(X, V);
+            else {
+                for (int i = 0; i < ATTRIBUTE_COUNT; i++)
+                    w[i] = rand(-0.005, 0.005);
+                w0 = rand(-0.005, 0.005);
+                leftNode.w0 = rand(-0.005, 0.005);
+                rightNode.w0 = rand(-0.005, 0.005);
+            }
 
             alpha = (tree.LEARNING_RATE + 0.0) / Math.pow(2, t + 1);
             learnParameters(X, V, alpha, tree, tree.EPOCH);
@@ -169,6 +176,120 @@ class Node {
             w0 = oldw0;
             y = w0;
         }
+    }
+
+    public void hardinit(ArrayList<Instance> X, ArrayList<Instance> V){
+        ArrayList<Double> sv = new ArrayList<>();
+        double total=0;
+
+
+        // (1) compute soft memberships
+        for (int j = 0; j < X.size(); j++) {
+            double t = 1;
+            Node m = this;
+            Node p;
+
+            while(m.parent != null) {
+                p = m.parent;
+                if (m.isLeft)
+                    t *= sigmoid(dotProduct(p.w, X.get(j).attributes) + p.w0);
+                else
+                    t *= (1-sigmoid(dotProduct(p.w, X.get(j).attributes) + p.w0));
+                m = m.parent;
+            }
+            sv.add(t);
+            total += t;
+        }
+
+        if (total <= 1) { // not enough data, init randomly
+            w = new double[X.get(0).attributes.length];
+            for (int i=0; i < w.length; i++)
+                w[i] = rand(-0.005, 0.005);
+            w0 = rand(-0.005, 0.005);
+            leftNode.w0 = rand(-0.005, 0.005);
+            rightNode.w0 = rand(-0.005, 0.005);
+            return;
+        }
+
+        int dim, bestDim=-1;
+        double errBest = -1;
+        double bestSplit = 0;
+        double bestw10 = 0, bestw20 = 0;
+        ArrayList<Double> bestw1 = new ArrayList<>();
+        ArrayList<Double> bestw2 = new ArrayList<>();
+
+        // (2) look for the best hard split
+        for (dim=0; dim < X.get(0).attributes.length; dim++)
+        {
+            ArrayList<Pair<Double,Integer>> f = new ArrayList<>();
+
+            double[] atts = new double[X.size()];
+            for (int i=0; i < X.size(); i++)
+                atts[i] = X.get(i).attributes[dim];
+            Util.ArrayIndexComparator comparator = new Util.ArrayIndexComparator(atts);
+            Integer[] indexes = comparator.createIndexArray();
+
+            Arrays.sort(indexes, comparator);
+
+            for (int i=0; i < X.size(); i++)
+                f.add(new Pair(X.get(indexes[i]).attributes[dim],indexes[i]));
+
+
+
+            double sp;
+            for (int i=0; i < f.size()-1; i++) {
+
+                if (f.get(i).getKey() == f.get(i + 1).getKey()) continue;
+                sp = 0.5 * (f.get(i).getKey() + f.get(i).getKey());
+
+                double w10,w20,left,right,lsum,rsum;
+
+                w10 = w20 = lsum = rsum = 0;
+                for (int j = 0; j <= i; j++) {
+                    w10 += X.get(f.get(j).getValue()).classValue * sv.get(f.get(j).getValue());
+                    lsum += sv.get(f.get(j).getValue());
+                }
+                w10 /= lsum;
+
+                for (int j=i+1; j < f.size(); j++) {
+                    w20 += X.get(f.get(j).getValue()).classValue * sv.get(f.get(j).getValue());
+                    rsum += sv.get(f.get(j).getValue());
+                }
+                w20 /= rsum;
+
+                // weighted MSE for regression and
+                // weighted Gini Impurity for classification
+                double errl = 0, errr = 0;
+                for (int j=0; j <= i; j++)
+                    errl += (w10 - X.get(f.get(j).getValue()).classValue)*(w10 - X.get(f.get(j).getValue()).classValue)*sv.get(f.get(j).getValue());
+                errl /= lsum;
+                for (int j=i+1; j < f.size(); j++)
+                    errr += (w20 - X.get(f.get(j).getValue()).classValue)*(w20 - X.get(f.get(j).getValue()).classValue)*sv.get(f.get(j).getValue());
+                errr /= rsum;
+
+                double a = lsum/(lsum+rsum+0.0);
+                double b = rsum/(lsum+rsum+0.0);
+
+                if (a*errl + b*errr < errBest || errBest == -1) {
+                    bestSplit = sp;
+                    bestDim = dim;
+                    errBest = a*errl + b*errr;
+                    bestw10 = w10;
+                    bestw20 = w20;
+                    //cout << errbest << endl;
+                }
+            }
+        }
+
+        // (3) init params according to best hard split
+
+        w = new double[X.get(0).attributes.length];
+        for (int i = 0; i < w.length; i++)
+            w[i] = rand(-0.005, 0.005);
+        w[bestDim] = -0.5;
+        w0 = bestSplit*0.5;
+        leftNode.w0 = bestw10;
+        rightNode.w0 = bestw20;
     }
 
     public String toString(int tab) {
